828186f9f19279f6e397fb02774f459e427dbd8f
javier-iesvae <114832717+javier-iesvae@users.noreply.github.com>
2022-10-02 19:25:50 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,39 @@
-basic.forever(function on_forever() {
-    for (let N = 0; N < 5; N++) {
-        for (let index = 0; index < 5; index++) {
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    for (let N = 0; N <= 4; N++) {
+        for (let index = 0; index <= 4; index++) {
             led.plot(N, index)
         }
+        basic.pause(500)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    for (let N = 0; N <= 4; N++) {
+        for (let index = 0; index <= 4; index++) {
+            led.plot(index, N)
+        }
+        basic.pause(500)
     }
 })
